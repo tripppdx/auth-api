03ef96d8dd638e417ab732a177ae1e639090abe4
Tripp Lucas <tripppdx@gmail.com>
2021-10-27 17:16:57 -0700

    initial commit

---
+++ src/modelinstance/index.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const { Sequelize, DataTypes } = require('sequelize');
+const clothesModel = require('../models/clothes/model.js');
+const foodModel = require('../models/food/model.js');
+const Collection = require('../models/data-collection.js');
+const userModel = require('../auth/models/users.js');
+
+const DATABASE_URL = process.env.DATABASE_URL || 'sqlite:memory:';
+
+const sequelize = new Sequelize(DATABASE_URL);
+const food = foodModel(sequelize, DataTypes);
+const clothes = clothesModel(sequelize, DataTypes);
+const users = userModel(sequelize, DataTypes);
+
+module.exports = {
+  db: sequelize,
+  food: new Collection(food),
+  clothes: new Collection(clothes),
+  users
+};
---
+++ src/server.js
@@ -0,0 +1,42 @@
+'use strict';
+
+// 3rd Party Resources
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+
+// Esoteric Resources
+const errorHandler = require('./error-handlers/500.js');
+const notFound = require('./error-handlers/404.js');
+const authRoutes = require('./auth/routes');
+const logger = require('./middleware/logger.js');
+const v1Routes = require('./routes/v1.js');
+
+// Prepare the express app
+const app = express();
+
+// App Level MW
+app.use(cors());
+app.use(morgan('dev'));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(logger);
+
+// Routes
+app.use(authRoutes);
+app.use('/api/v1', v1Routes);
+
+// Catchalls
+app.use(notFound);
+app.use(errorHandler);
+
+module.exports = {
+  server: app,
+  start: (port) => {
+    app.listen(port, () => {
+      console.log(`Server Up on ${port}`);
+    });
+  },
+};
